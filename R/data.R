1e9db2e4dc86908e698668b90ffbeb7d1b923b01
Christian Ryan <drchristianryan@gmail.com>
2021-01-01 17:34:41 +0000

    Updated data folder with Edwards 2015

--- R/data.R
+++ R/data.R
@@ -3,3 +3,10 @@
 #'
 #' @author Larson et al.
 'Larson_2015'
+
+
+#' This is the Edwards dataset
+#' @usage data(Edwards_2015)
+#'
+#' @author Edwards et al.
+'Edwards_2015'
---
+++ R/df.R
@@ -0,0 +1,11 @@
+library(devtools)
+# load("inst/extdata/Edwards_2015.Rdata")
+
+# Rename df to data file name
+# Edwards_2015 <- df
+
+# send to data folder
+# use_data(Edwards_2015)
+
+# clear environment
+# rm(list = ls())
